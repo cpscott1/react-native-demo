ad1872a90e1e4efdf428e21e1f00de150b6e5a23
cpscott1 <thacam18@gmail.com>
2019-09-02 21:45:34 -0500

    added some styling

--- react-native-project/App.js
+++ react-native-project/App.js
@@ -3,17 +3,23 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+    <View style={styles.appContainer}>
+      <View>
+        <Text style={styles.title}>Hello Students</Text>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center"
   },
-});
+  title: {
+    color: "white",
+    fontSize: 24
+  }
+})
